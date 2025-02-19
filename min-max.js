1674ef14484a81a06b7c56ee99aeedbb4481d7cc
idmossab <idmossab@gmail.com>
2025-02-19 18:59:43 +0100

    min-maxx

--- min-max.js
+++ min-max.js
@@ -1,5 +1,5 @@
 const max = (nbr1, nbr2) => nbr1 > nbr2 ? nbr1 : nbr2;
-const min = (nbr1, nbr2) => !max(nbr1, nbr2) ? nbr1 : nbr2;
+const min = (nbr1, nbr2) => nbr1 < nbr2 ? nbr1 : nbr2;
 
 console.log(max(5, 3));
-console.log(min(5, 3));
+console.log(min(0, -2));
