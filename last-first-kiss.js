0a15414ae5ead571280041269a76dcb8c25d0ddf
idmossab <idmossab@gmail.com>
2025-02-19 16:13:56 +0100

    last-fisrt-kiss1

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,18 +1,3 @@
-const first = (arg) => {
-    if (typeof arg === 'string' || Array.isArray(arg)) {
-        return arg[0];
-    }
-    return 'arg must be string or array';
-};
-const last = (arg) => {
-    if (typeof arg === 'string' || Array.isArray(arg)) {
-        return arg[arg.length - 1];
-    }
-    return 'arg must be string or array';
-};
-const kiss = (arg) => {
-    if (typeof arg === 'string' || Array.isArray(arg)) {
-        return (arg[arg.length - 1] + "" + arg[0]).toString();
-    }
-    return 'arg must be string or array';
-};
+const first = (arg) => arg[0];
+const last = (arg) => arg[arg.length - 1];
+const kiss = (arg) => [last(arg), first(arg)];
