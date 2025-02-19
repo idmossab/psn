a18bc5ce5b198f8a4bedbca8aa2efa5aba32e988
idmossab <idmossab@gmail.com>
2025-02-19 18:43:25 +0100

    absss

--- abs.js
+++ abs.js
@@ -1,3 +1,3 @@
-const isPositive = (nbr) => nbr >= 0 ? true : false;
-const abs = (nbr) => !isPositive(nbr) && nbr != 0 ? -nbr : nbr;
+const isPositive = (nbr) => nbr > 0 ? true : false;
+const abs = (nbr) => (!isPositive(nbr) && nbr != 0) ? -nbr : nbr;
 console.log(abs(0));
