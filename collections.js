12b240bad7946640d009d5272ae785cb748cb683
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-20 17:10:14 +0100

    collections

--- collections.js
+++ collections.js
@@ -1,13 +1,13 @@
 const arrToSet = (arr) => new Set(arr);
 const arrToStr = (arr) => arr.join('');
 //const setToArr = (set) => Array.from(set);
-const setToArr = (set) => [...set].join(' ').split(' ');
-const setToStr = (set) => [...set].join(' ');
-//const setToStr=(set)=>Array.from(set).join(' ');
-const strToArr = (str) => str.split(' ');
+const setToArr = (set) => [...set];
+const setToStr = (set) => [...set].join('');
+//const setToStr=(set)=>Array.from(set).join('');
+const strToArr = (str) => str.split('');
 const strToSet = (str) => new Set(str);
 // const mapToObj = (map) => Object.fromEntries(map);
-const mapToObj1 = (map) => {
+const mapToObj = (map) => {
     const obj = {};
     map.forEach((value, key) => {
         obj[key] = value; // Add each key-value pair to the new object
@@ -16,16 +16,20 @@ const mapToObj1 = (map) => {
 };
 const objToArr = (obj) => Object.values(obj);
 //const objToMap = (obj) => new Map(Object.entries(obj));
-const objToMap = (obj) => new Map(Object.entries(obj));
-//const arrToObj=(arr)=>;
-//const strToObj=(str)=>;
+const objToMap = (obj) => {
+    const map = new Map();
+    Object.keys(obj).forEach((key) => {
+        map.set(key, obj[key]);
+    });
+    return map;
+};
+const arrToObj = (arr) => obj = {...arr };
+const strToObj = (str) => obj = Object.assign({}, str);
 
-// let map = new Map()
-// map.set("user1", "Joe")
-// map.set("user2", "Rogan")
-// map.set("user3", "Jack")
 
 
-console.log(objToMap({ name: 'John', age: 30, city: 'New York' }));
+//console.log(strToArr("hello world"))
+
+console.log(strToObj("hello"));
 
 //console.log(strToSet("hello how are"));
