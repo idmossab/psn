4b2e3be50044e728b79d62fcf1b23b127ff6065f
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-21 15:22:18 +0100

    keep-cut

--- keep-cut.js
+++ keep-cut.js
@@ -3,5 +3,5 @@ const cutLast = (arg) => arg.slice(0, arg.length - 2);
 const cutFirstLast = (arg) => arg.slice(2, arg.length - 2);
 const keepFirst = (arg) => arg.slice(0, 2);
 const keepLast = (arg) => arg.slice(arg.length - 2);
-const keepFirstLast = (arg) => keepFirst(arg) + "" + keepLast(arg);
-console.log(keepFirstLast("helllo"));
+const keepFirstLast = (arg) => arg.length <= 4 ? arg : keepFirst(arg) + "" + keepLast(arg);
+console.log(keepFirstLast("af5"));
