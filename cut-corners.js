ed5fbc27dc7fe29a75b4e5985c25aa242fc9133c
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-21 16:58:44 +0100

    cut-corners3

--- cut-corners.js
+++ cut-corners.js
@@ -43,3 +43,16 @@ const ceil = (nbr) => {
     let res = divide(nbr, 1);
     return nbr >= 0 ? res + 1 : res;
 };
+const floor = (nbr) => {
+    let mod = modulo(nbr, 1);
+    if (mod == 0) return nbr;
+    let res = divide(nbr, 1);
+    return nbr >= 0 ? res : res - 1;
+
+};
+const trunc = (nbr) => {
+    let mod = modulo(nbr, 1);
+    if (mod == 0) return nbr;
+    let res = divide(nbr, 1);
+    return res
+};
