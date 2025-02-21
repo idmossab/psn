fbe2f480695c58eb061d3be0aa0feab53c546882
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-21 12:11:20 +0100

    elementary v2

--- elementary.js
+++ elementary.js
@@ -1,47 +1,26 @@
 const multiply = (a, b) => {
     let mult = 0;
-    let sign = 1;
-    if (a < 0 && b > 0 || a > 0 && b < 0) {
-        sign = -1;
-    }
-    if (b < 0 || a < 0) {
-        if (a < 0) {
-            a = -a;
-        }
-        if (b < 0) {
-            b = -b;
-        }
-    };
+    let sign = (a < 0 && b > 0 || a > 0 && b < 0);
+    a = Math.abs(a);
+    b = Math.abs(b);
     //console.log("a:", a, "b:", b);
     while (b > 0) {
         mult += a;
         b--;
     }
-    if (sign < 0) {
-        mult = -mult
-    }
     // console.log("a:", a, "b:", b, "mult", mult);
-    return mult;
+    return sign ? -mult : mult;
 };
 const divide = (a, b) => {
     let count = 0;
-    let sign = 1;
-    if (b == 0) return undefined;
-    if (b < 0 && a < 0) {
-        a = -a;
-        b = -b;
-    } else if (a < 0) {
-        a = -a;
-        sign = -1;
-    } else if (b < 0) {
-        b = -b;
-        sign = -1;
-    };
+    let sign = (a < 0 && b > 0 || a > 0 && b < 0);
+    a = Math.abs(a);
+    b = Math.abs(b);
     while (a >= b) {
         a -= b;
         count++;
     };
-    return multiply(sign, count);
+    return sign ? -count : count;
 };
 const modulo = (a, b) => {
     let div = divide(a, b);
@@ -49,5 +28,5 @@ const modulo = (a, b) => {
     return a - mod;
 };
 console.log(multiply(-22, 123));
-console.log(divide(13, 2));
+console.log(divide(0, -2));
 console.log(modulo(13, 2));
