e59a9665de9bf47f8e56ff3467bc5f3c8c66351f
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-21 16:46:12 +0100

    cut-corners1

--- cut-corners.js
+++ cut-corners.js
@@ -1,31 +1,3 @@
-const multiply = (a, b) => {
-    let mult = 0;
-    let sign = (a < 0 && b > 0 || a > 0 && b < 0);
-    a = Math.abs(a);
-    b = Math.abs(b);
-    while (b > 0) {
-        mult += a;
-        b--;
-    }
-    return sign ? -mult : mult;
-};
-const divide = (a, b) => {
-    let count = 0;
-    let sign = (a < 0 && b > 0 || a > 0 && b < 0);
-    a = Math.abs(a);
-    b = Math.abs(b);
-    while (a >= b) {
-        a -= b;
-        count++;
-    };
-    return sign ? -count : count;
-};
-const modulo = (a, b) => {
-    let div = divide(a, b);
-    let mod = multiply(b, div);
-    return a - mod;
-};
-
 const round = (nbr) => {
     let mod = modulo(nbr, 1);
     if (mod == 0) return nbr;
