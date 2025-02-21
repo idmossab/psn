545529820872079d1b43dc1eaeb1a1ae327c3ad6
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-21 12:53:07 +0100

    index-of-js

--- index-of.js
+++ index-of.js
@@ -1,11 +1,14 @@
 const indexOf = (arr, str, start = 0) => {
+    if (start > arr.length || start < 0) return -1
     for (let i = start; i < arr.length; i++) {
         if (arr[i] == str) return i;
     }
     return -1;
 };
 const lastIndexOf = (arr, str, start = arr.length) => {
-    for (let i = start - 1; i >= 0; i--) {
+    if (start > arr.length || start < 0) return -1
+    console.log("start", start);
+    for (let i = start; i >= 0; i--) {
         if (arr[i] == str) return i;
     }
     return -1;
@@ -16,6 +19,6 @@ const includes = (arr, str) => {
 };
 
 
-console.log(indexOf(["hello", "how are you", "word"], "word"));
-console.log(lastIndexOf(["hello", "how are you", "word"], "hello", 1));
+//console.log(indexOf(["hello", "how are you", "word"], "word"));
+console.log(lastIndexOf([0, 0, "t", "t"], "t", 3));
 console.log(includes(["hello", "how are you", "word"], "hello"));
