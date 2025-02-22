3f04900568f8d16679d0a0b892683e9c8057ba1f
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-22 14:33:46 +0100

    nasa

---
+++ nasa.js
@@ -0,0 +1,10 @@
+const nasa = (n) => {
+  let str = "";
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) str += "NASA";
+    else if (i % 3 == 0) str += "NA";
+    else if (i % 5 == 0) str += "SA";
+    else str += i;
+  }
+  return str;
+};
