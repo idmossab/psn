59f243034b9645d9e6813c59dcffcb7edfabbd67
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-22 11:58:43 +0100

    unbreakable

---
+++ unbreakable.js
@@ -0,0 +1,27 @@
+const split=(str,sp="")=>{
+    let arr=[];
+    if(sp=="")return arr=[...str];
+    res="";
+    for(let i=0;i<=str.length;i++){
+        if(str[i]==sp||i==str.length){
+            arr.push(res);
+            res="";
+            continue
+        }
+        res+=str[i];
+    }
+    return arr;
+}
+const join=(arr,sp="")=>{
+    let str="";
+    for(let i=0;i<arr.length;i++){
+        str+=arr[i];
+        if(i!=arr.length-1){
+            str+=sp;
+        }
+        
+    }
+return str;
+}
+console.log(split("hello word"));
+console.log(join(['Fire', 'Air', 'Water']));
