a8afc5962c94deda357ed53345d6617a29fc7a2b
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-22 18:09:02 +0100

    pyramide1

--- pyramid.js
+++ pyramid.js
@@ -1,13 +1,22 @@
-const pyramid = (str, nbr) => {
+// const pyramid = (str, nbr) => {
+//   let res = "";
+//   for (let i = 1; i <= nbr; i++) {
+//     for (let j = nbr - i; j > 0; j--) {
+//       res += " ".repeat(str.length);
+//     }
+//     for (let k = 1; k <= 2 * i - 1; k++) {
+//       res += str;
+//     }
+//     if (i != nbr) res += "\n";
+//   }
+//   return res;
+// };
+
+function pyramid(str, nbr) {
   let res = "";
-  for (let i = 1; i <= nbr; i++) {
-    for (let j = nbr - i; j > 0; j--) {
-      res += " ".repeat(str.length);
-    }
-    for (let k = 1; k <= 2 * i - 1; k++) {
-      res += str;
-    }
-    if (i != nbr) res += "\n";
+  for (let i=1;i<=nbr;i++){
+    res+=" ".repeat((nbr-i)*str.length)+str.repeat(2*i-1);
+    if(i!=nbr)res+="\n"
   }
-  return res;
-};
+  return res
+}
