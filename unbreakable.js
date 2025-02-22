e03d280ce88b2731a2ed66cc87e3accfe7b737e2
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-22 12:57:20 +0100

    unbrekable

--- unbreakable.js
+++ unbreakable.js
@@ -1,26 +1,30 @@
 const split = (str, sp = "") => {
-    let arr = [];
-    if (sp == "") return arr = [...str];
-    let res = "";
-    for (let i = 0; i <= str.length; i++) {
-        if (str[i] == sp || i == str.length) {
-            arr.push(res);
-            res = "";
-            continue
-        }
-        res += str[i];
+  let arr = [];
+  if (sp == "") return (arr = [...str]);
+  let strSp = "";
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] === sp[0]) {
+      if (str.slice(j, j + sp.length) == sp) {
+        arr.push(strSp);
+        strSp = "";
+        j += sp.length-1;
+      }
+    } else {
+      strSp += str[j];
     }
-    return arr;
-}
+  }
+  if (strSp !== "undefined" && strSp!=sp) {
+    arr.push(strSp);
+  }
+  return arr;
+};
 const join = (arr, sp = "") => {
-    let str = "";
-    for (let i = 0; i < arr.length; i++) {
-        str += arr[i];
-        if (i != arr.length - 1) {
-            str += sp;
-        }
-
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i];
+    if (i != arr.length - 1) {
+      str += sp;
     }
-    return str;
-}
-console.log(split("a b c"," "))
+  }
+  return str;
+};
