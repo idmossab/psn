b97c2fc51ce93a18e8769d91c3985ef119259923
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-22 12:01:47 +0100

    unbreakable2

--- unbreakable.js
+++ unbreakable.js
@@ -1,25 +1,25 @@
-const split=(str,sp="")=>{
-    let arr=[];
-    if(sp=="")return arr=[...str];
-    res="";
-    for(let i=0;i<=str.length;i++){
-        if(str[i]==sp||i==str.length){
+const split = (str, sp = "") => {
+    let arr = [];
+    if (sp == "") return arr = [...str];
+    res = "";
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] == sp || i == str.length) {
             arr.push(res);
-            res="";
+            res = "";
             continue
         }
-        res+=str[i];
+        res += str[i];
     }
     return arr;
 }
-const join=(arr,sp="")=>{
-    let str="";
-    for(let i=0;i<arr.length;i++){
-        str+=arr[i];
-        if(i!=arr.length-1){
-            str+=sp;
+const join = (arr, sp = "") => {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i];
+        if (i != arr.length - 1) {
+            str += sp;
         }
-        
+
     }
-return str;
+    return str;
 }
