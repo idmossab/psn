8da47d873ddcd6e99b43aec8b21ef107799751ee
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-23 10:39:56 +0000

    itd-a-match

--- Loop/triangle.js
+++ Loop/triangle.js
@@ -4,9 +4,7 @@ const triangle = (str, nbr) => {
     for (let j = 0; j < i + 1; j++) {
       res += str;
     }
-    if (i != nbr - 1) {
-      res += "\n";
-    }
-  }
+    if (i != nbr - 1)res += "\n"; 
+ }
   return res;
 };
