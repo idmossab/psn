eadab0a1554c03150dc30e4d29f0adf540245b40
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-23 11:07:18 +0000

    litter1

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,6 @@
 const letterSpaceNumber=(str)=>{
-    const reg=/\w\s\d{1}/g;
+    const reg=/\w\s\d{1}\b/g;
     let result = str.match(reg) || [];
     return result
 };
-//console.log(letterSpaceNumber('example 1, example 20'))
+//console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
