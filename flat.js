b8b30bf5ef5de4871d69b5f6714e75290ed41488
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-23 10:28:37 +0000

    flat

---
+++ flat.js
@@ -0,0 +1,16 @@
+const flat=(arr,nbr=1)=>{
+    let res=[];
+    if (nbr ==Infinity){
+        nbr = arr.length+1
+    }
+    while(nbr>=1){
+        for(let item of arr){
+            Array.isArray(item)?res.push(...item):res.push(item);
+        }
+        arr=res
+        res=[]
+        nbr--;
+    }
+    return arr;
+}
+//console.log(flat([1, [2, [3], [4, [5]]]],Infinity));
