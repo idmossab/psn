0b97c661fb5f140dd4b36843eb11b8903b9f0f97
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 17:37:08 +0000

    rebeca1

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,9 +1,16 @@
-const isFriday =(day)=> day.getDay()==5?true:false;
-const isWeekend=(day)=>day.getDay()==6||0?true:false;
-const isLeapYear=(year) => ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-const isLastDayOfMonth=(date) => {
-    const yr=date.getFullYear()
-    const mth=date.getMonth()
-    const endOfMonth = new Date(yr, mth + 1, 0).getDate()
-    return date.getDate()==endOfMonth
+const isFriday = (date) => (date.getDay() == 5 ? true : false);
+const isWeekend = (date) => (date.getDay() == 6 || 0 ? true : false);
+const isLeapYear = (date) =>
+  date.getFullYear() % 4 == 0
+    ? date.getFullYear() % 100 == 0
+      ? date.getFullYear() % 400 == 0
+        ? true
+        : false
+      : true
+    : false;
+const isLastDayOfMonth = (date) => {
+  const yr = date.getFullYear();
+  const mth = date.getMonth();
+  const endOfMonth = new Date(yr, mth + 1, 0).getDate();
+  return date.getDate() == endOfMonth;
 };
