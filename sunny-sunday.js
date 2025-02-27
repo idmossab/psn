4b4c64400ba4fa487e3f395fb04ecba634b9582c
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 20:46:55 +0000

    sanny-sanday

---
+++ sunny-sunday.js
@@ -0,0 +1,13 @@
+const sunnySunday = (date) => {
+    const dateBase = new Date('0001-01-01')
+    const days = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+    }
+    datDif = (date - dateBase) / (1000 * 60 * 60 * 24)
+    return days[datDif % 6]
+}
