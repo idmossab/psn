aa73e16681cbd3d57b891f83e069a58401d386dd
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 19:05:11 +0000

    count-leap-year1

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,20 +1,20 @@
-const countLeapYears=(date)=>{
-    let yr=date.getFullYear();
-    let leap=0
-    for(let i=1;i<yr;i++){
-        if(i%4==0){
-            if(i%100==0){
-                if(i%400==0){
+const countLeapYears = (date) => {
+    let yr = date.getFullYear();
+    let leap = 0
+    for (let i = 1; i < yr; i++) {
+        if (i % 4 == 0) {
+            if (i % 100 == 0) {
+                if (i % 400 == 0) {
                     leap++
                 }
-            }else{
+            } else {
                 leap++
             }
         }
     }
-   // console.log(yr)
+    // console.log(yr)
     return leap
 
 }
 
-console.log(countLeapYears(new Date('1664-08-09')));
+//console.log(countLeapYears(new Date('1664-08-09')));
