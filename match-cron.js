8bfb93dad142e318c92ec480388b60a4fabf6684
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 18:48:05 +0000

    match-cron2

--- match-cron.js
+++ match-cron.js
@@ -1,12 +1,8 @@
 const matchCron = (cron, date) => {
-  if (cron.length != 9) return;
+  if (cron.length != 9) return false;
   const cr = cron.split(' ');
   //console.log(date.getMonth())
-  if (cr[0] == date.getMinutes()) return true;
-  if (cr[1] == date.getHours()) return true;
-  if (cr[2] == date.getDate()) return true
-  if (cr[3] == date.getMonth()+1) return true
-  if (cr[4] == date.getDay()) return true
+  if ((cr[0] == date.getMinutes() || cr[0] == '*') && (cr[1] == date.getHours() || cr[1] == '*') && (cr[2] == date.getDate() || cr[2] == '*') && (cr[3] == date.getMonth() + 1 || cr[3] == '*') && (cr[4] == date.getDay() || cr[4] == '*')) return true;
   return false
 };
-//console.log(matchCron('* * * 2 *', new Date('2021-02-01 00:00:00')))
+//console.log(matchCron('* * * * *', new Date()))
