5244c98afe5b54df9ba2d18b479832b8264eda89
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 15:01:19 +0000

    get-some time

---
+++ get-some-time.js
@@ -0,0 +1,16 @@
+const firstDayWeek = (nbrWeek, year) => {
+  let dateBase = new Date(0);
+  dateBase.setFullYear(Number(year));
+  if (nbrWeek == 1) {
+    return `01-01-${year}`;
+  }
+  const day = new Date(
+    dateBase.getTime() + (nbrWeek - 1) * 7 * 24 * 60 * 60 * 1000
+  );
+  //console.log("before", day);
+  while (day.getDay() != 1) {
+    day.setDate(day.getDate() -1);
+  }
+  //console.log("after", day);
+  return day.toISOString().slice(0,10).split('-').reverse().join('-');
+};
