c5b7f9b4fee84f64f9145b809814549c808f0b88
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 11:53:24 +0000

    date-is3

--- date-is.js
+++ date-is.js
@@ -1,5 +1,5 @@
 //Date.now()==>result ms that we do new date for check format
-const isValid=(date)=> (new Date(date) instanceof Date && !isNaN(date));
+const isValid=(date)=> (new Date(date) instanceof Date && !isNaN(date) && date!="");
 const isAfter=(date1,date2)=> (date1>date2);
 const isBefore=(date1,date2)=> (date1<date2);
 const isFuture=(date)=> (isValid(date) && (isAfter(date,new Date())));
