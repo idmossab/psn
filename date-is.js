b68272c4efee03fddb5a9c94d0683a8474a0fd20
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 11:36:43 +0000

    date-is

--- date-is.js
+++ date-is.js
@@ -1,5 +1,5 @@
 const isValid=(date)=> date instanceof Date && !isNaN(date);
 const isAfter=(date1,date2)=> date1>date2;
 const isBefore=(date1,date2)=> date1<date2;
-
-console.log(isAfter(new Date('0001-01-02'),new Date('0001-01-01')))
+const isFuture=(date)=> (isValid(date) && (isAfter(date,new Date())))?true:false;
+const isPast=(date)=>(isValid(date) && (isBefore(date,new Date())))?true:false;
