05bae0c66f185f43ac36c935ca2de49429334954
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-27 11:39:51 +0000

    date-is1

--- date-is.js
+++ date-is.js
@@ -1,5 +1,5 @@
-const isValid=(date)=> date instanceof Date && !isNaN(date);
-const isAfter=(date1,date2)=> date1>date2;
-const isBefore=(date1,date2)=> date1<date2;
-const isFuture=(date)=> (isValid(date) && (isAfter(date,new Date())))?true:false;
-const isPast=(date)=>(isValid(date) && (isBefore(date,new Date())))?true:false;
+const isValid=(date)=> (date instanceof Date && !isNaN(date));
+const isAfter=(date1,date2)=> (date1>date2);
+const isBefore=(date1,date2)=> (date1<date2);
+const isFuture=(date)=> (isValid(date) && (isAfter(date,new Date())));
+const isPast=(date)=>(isValid(date) && (isBefore(date,new Date())));
