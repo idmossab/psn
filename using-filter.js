263e2f4b0e97df1fcbd5227e8349ff9fbbb1543d
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-28 15:59:39 +0000

    filter4

--- using-filter.js
+++ using-filter.js
@@ -23,40 +23,10 @@ const filter1DistinctVowel = (arr) => {
         }
     })
 }
-const filter1Vowels = (arr) => {
-    const reg = /[aeiou]/gi
-    return arr.filter(str => {
-        let a = str.match(reg)
-        //console.log(a)
-        if (a != null) a = a.join('')
-        //console.log(a)
-        return a.length > 1
-    })
-}
+
 const multiFilter = (arr) => {
-    return arr.filter(obj => 
-        obj.capital.length >= 8 && filterStartVowel([obj.name]).length==0&& filter1Vowels((obj.tag).split()) && obj.region != "South"
+    const reg = /[aeiou]/gi
+    return arr.filter(obj =>
+        obj.capital.length >= 8 && !reg.test(obj.name[0]) && reg.test(obj.tag) && obj.region != "South"
     )
 }
-console.log(multiFilter([
-    { tag: 'CA', name: 'iCalifornia', capital: 'Sacramento', region: 'West' },
-    { tag: 'HI', name: 'Hawaii', capital: 'Honolulu', region: 'West' },
-    {
-        tag: 'MO',
-        name: 'Missouri',
-        capital: 'Jefferson City',
-        region: 'Midwest',
-    },
-    {
-        tag: 'PA',
-        name: 'Pennsylvania',
-        capital: 'Harrisburg',
-        region: 'Northeast',
-    },
-    {
-        tag: 'RI',
-        name: 'Rhode Island',
-        capital: 'Providence',
-        region: 'Northeast',
-    },
-]))
