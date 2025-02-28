76496ed731bdd0b0a2270bd5f4a28430d467a944
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-28 01:23:41 +0000

    usingmap

--- using-map.js
+++ using-map.js
@@ -1,27 +1,27 @@
 const citiesOnly = (arr) => arr.map(obj => obj.city)
+
 const upperCasingStates = (arr) => {
     const reg = /\b\w/g
     return arr.map(obj => obj.replace(reg, reg => reg.toUpperCase()))
 }
+
 const fahrenheitToCelsius = (arr) => {
-    let degree = arr.map(obj => obj = obj.split('°').slice(0, 1)).flat(1)
-    let conv = degree.map(c => parseInt(c))
-    let dg = conv.map(cv => cv = Math.round((cv - 32) * (5 / 9)).toString())
-    return dg.map(item => item.replace(item, item + "°C"))
-}
-const trimTemp = (arr) => {
-    const reg = /[^\s]/g
-    return arr.map(obj => obj.temperature.match(reg).join(''))
-}
-const tempForecasts = (arr) => {
-    return arr.map(obj => fahrenheitToCelsius(obj.temperature))
+    return arr.map(temp => {
+        let fahrenheit = parseInt(temp.replace('°F', ''));
+        let celsius = Math.round((fahrenheit - 32) * 5 / 9);
+        return celsius + "°C";
+    });
 }
 
-console.log(tempForecasts([
-    {
-      city: 'Pasadena',
-      temperature: ' 101 °F',
-      state: 'california',
-      region: 'West',
-    },
-  ]))
+const trimTemp = (arr) => {
+    return arr.map(obj => obj.temperature.trim());
+}
+
+const tempForecasts = (arr) => {
+    return arr.map(element => {
+        let city = element.city;
+        let state = upperCasingStates([element.state])[0];
+        let temp = fahrenheitToCelsius([element.temperature])[0];
+        return `${temp}elsius in ${city}, ${state}`;
+    });
+}
