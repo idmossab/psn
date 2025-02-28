c1b85aae733f0d029c04dae945b652af78f2dfe2
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-28 19:19:25 +0000

    maper

--- mapper.js
+++ mapper.js
@@ -9,7 +9,12 @@ const map = (arr, fnc) => {
 const flatMap = (arr, fnc) => {
     let res = []
     for (let i = 0; i < arr.length; i++) {
-        res.push(...fnc(arr[i], i, arr))
+        var v = arr[i];
+        if (v instanceof Array) {
+            Array.prototype.push.apply(arr, v.flatten());
+        } else {
+            res.push(v);
+        }
     }
     return res
 }
