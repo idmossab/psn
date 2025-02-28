cf398c8d3becf904a6f7202f1d23892c7b8a7733
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-28 16:55:27 +0000

    filter6

--- using-filter.js
+++ using-filter.js
@@ -13,13 +13,6 @@ const filter5Vowels = (arr) => {
         return a.length >= 5
     })
 }
-
-const filter1Vowels = (arr) => {
-    return arr.filter(str => {
-        return str.match(/a|e|u|i|o/gi)
-    })
-}
-
 const filter1DistinctVowel = (arr) => {
     const reg = /[aeiou]/gi
     return arr.filter(str => {
@@ -30,31 +23,12 @@ const filter1DistinctVowel = (arr) => {
         }
     })
 }
-
+const filter1Vowels = (arr) => {
+    const reg = /[aeiou]/gi
+    return arr.filter(str => str.match(reg))
+}
 const multiFilter = (arr) => {
-    return arr.filter(obj => 
-        obj.capital.length >= 8 && filterStartVowel([obj.name]).length==0 && filter1Vowels(([obj.tag])).length >= 1 &&  obj.region != "South"
+    return arr.filter(obj =>
+        obj.capital.length >= 8 && filterStartVowel([obj.name]).length == 0 && filter1Vowels(([obj.tag])).length >= 1 && obj.region != "South"
     )
 }
-console.log(multiFilter([
-    { tag: 'CA', name: 'California', capital: 'Sacramento', region: 'West' },
-    { tag: 'HI', name: 'Hawaii', capital: 'Honolulu', region: 'West' },
-    {
-        tag: 'MO',
-        name: 'Missouri',
-        capital: 'Jefferson City',
-        region: 'Midwest',
-    },
-    {
-        tag: 'PAU',
-        name: 'Pennsylvania',
-        capital: 'Harrisburg',
-        region: 'Northeast',
-    },
-    {
-        tag: 'Rr',
-        name: 'Rhode Island',
-        capital: 'Providence',
-        region: 'Northeast',
-    },
-]))
