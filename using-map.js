94a9d493dbf48ac0f9a0a51ee3f2c1114978e3ee
Mossab Idbenkellal <idmossab@gmail.com>
2025-02-28 01:31:33 +0000

    usingmap2

--- using-map.js
+++ using-map.js
@@ -1,27 +1,29 @@
-const citiesOnly = (arr) => arr.map(obj => obj.city)
+const citiesOnly = (arr) => {
+    return arr.map(element => element.city);
+};
 
 const upperCasingStates = (arr) => {
-    const reg = /\b\w/g
-    return arr.map(obj => obj.replace(reg, reg => reg.toUpperCase()))
-}
+    return arr.map(ele => ele.replace(/(?<=^|\s)([a-z])/g, match => match.toUpperCase()));
+};
 
 const fahrenheitToCelsius = (arr) => {
-    return arr.map(temp => {
-        let fahrenheit = parseInt(temp.replace('°F', ''));
+    return arr.map(element => {
+        let fahrenheit = parseInt(element.replace('°F', '').trim()); 
         let celsius = Math.floor((fahrenheit - 32) * 5 / 9);
-        return celsius + "°C";
+        return celsius + "°C"; 
     });
-}
+};
 
 const trimTemp = (arr) => {
-    return arr.map(obj => obj.temperature.trim());
-}
+    let arrMdf = arr.map(element => element.temperature.replaceAll(' ', '')); 
+    for (let i = 0; i < arr.length; i++) arr[i].temperature = arrMdf[i];   
+    return arr;
+};
 
 const tempForecasts = (arr) => {
-    return arr.map(element => {
-        let city = element.city;
-        let state = upperCasingStates([element.state])[0];
-        let temp = fahrenheitToCelsius([element.temperature])[0];
-        return `${temp}elsius in ${city}, ${state}`;
+    return trimTemp(arr).map(element => {
+        let celsiusTemp = fahrenheitToCelsius([element.temperature])[0];   
+        let state = upperCasingStates([element.state])[0];      
+        return `${celsiusTemp}elsius in ${element.city}, ${state}`; 
     });
-}
+};
