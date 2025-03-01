da6979170a993f2c1f737402628f0c454a7dd2bf
Mossab Idbenkellal <idmossab@gmail.com>
2025-03-01 15:14:37 +0000

    edit hasCity

--- call-me-maybe/has-city.js
+++ call-me-maybe/has-city.js
@@ -1,8 +1,3 @@
-const hasCity = (country, arr) => {
-    return (city) => {
-        if (arr.includes(city)) return city + " is a city from " + country
-        return city + " is not a city from " + country
-    }
-}
+const hasCity = (country, arr) => (city) => arr.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`;
 
 //console.log(hasCity("maroc",["tanger","casa","oujda"])("oujda"))
