0fa6849c6c0b4ad63b76513a5da1468566b3d267
Mossab Idbenkellal <idmossab@gmail.com>
2025-03-01 13:29:11 +0000

    seet-curry

---
+++ sweet-curry.js
@@ -0,0 +1,11 @@
+const mult2 = (nb1) => (nb2) => {
+    return nb1 * nb2
+}
+
+const add3 = (nb1) => (nb2) => (nb3) => {
+    return nb1 + nb2 + nb3
+}
+
+const sub4 = (nb1) => (nb2) => (nb3) => (nb4) => {
+    return nb1 - nb2 - nb3 - nb4
+}
