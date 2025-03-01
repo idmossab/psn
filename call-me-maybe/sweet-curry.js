0d0c868feb46d7576e03f6921c512e1868624eec
Mossab Idbenkellal <idmossab@gmail.com>
2025-03-01 15:15:42 +0000

    edit sweet-curry

--- call-me-maybe/sweet-curry.js
+++ call-me-maybe/sweet-curry.js
@@ -1,12 +1,5 @@
-const mult2 = (nb1) => (nb2) => {
-    return nb1 * nb2
-}
+const mult2 = (nb1) => (nb2) => (nb1 * nb2)
+const add3 = (nb1) => (nb2) => (nb3) =>  (nb1 + nb2 + nb3)
+const sub4 = (nb1) => (nb2) => (nb3) => (nb4) =>  (nb1 - nb2 - nb3 - nb4)
 
-const add3 = (nb1) => (nb2) => (nb3) => {
-    return nb1 + nb2 + nb3
-}
-
-const sub4 = (nb1) => (nb2) => (nb3) => (nb4) => {
-    return nb1 - nb2 - nb3 - nb4
-}
 //console.log(sub4(2)(5)(5)(5))
