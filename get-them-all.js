8bf8dd692c5327baf5e5dfa3a1f1b18487ff35fd
Mossab Idbenkellal <idmossab@gmail.com>
2025-03-01 17:51:57 +0000

    get-them

---
+++ get-them-all.js
@@ -0,0 +1,22 @@
+export const getArchitects = () => {
+    const architects = document.querySelectorAll("a");
+    const non_architects = document.querySelectorAll("span");
+     return [architects,non_architects]
+}
+export const getClassical = () => {
+    const getClassical = document.querySelectorAll(".classical");
+    //  return [[],[]]
+    
+}
+
+export const getActive = () => {
+    //  return [[],[]]
+    
+}
+
+export const getBonannoPisano = () => {
+    //  return [html,[]]
+    
+}
+
+
